1f816e72260167fc7cd3f162bf87038719f75bc5
Lujain Abusada <Lujain.Abusada@gmail.com>
2022-02-05 20:38:55 +0200

    Create the data model

---
+++ DoctorWho.Domain/Episode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorWho.Domain
+{
+    public class Episode
+    {
+        public Episode()
+        {
+            EpisodeCompanions = new List<EpisodeCompanion>();
+            EpisodeEnemies = new List<EpisodeEnemy>();
+            Doctor = new Doctor();
+            Author = new Author();
+        }
+
+        public int EpisodeId { get; set; }
+        public int SeriesNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string EpisodeType { get; set; }
+        public string Title { get; set; }
+        public string EpisodeDate { get; set; }
+        public string Notes { get; set; }
+        public Doctor Doctor { get; set; }
+        public Author Author { get; set; }
+        public List<EpisodeCompanion> EpisodeCompanions { get; set; }
+        public List<EpisodeEnemy> EpisodeEnemies { get; set; }
+    }
+}
